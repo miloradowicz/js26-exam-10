5b21d66c6a166986e65ca40b3dcf18fc7a59c5a2
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-21 16:47:27 +0600

    api updated: cors added

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import config from './config';
 import mysqlDb from './mysqlDb';
@@ -8,6 +9,7 @@ import commentsRouter from './routers/comments';
 const app = express();
 const port = config.express.port;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(config.publicPath));
 
